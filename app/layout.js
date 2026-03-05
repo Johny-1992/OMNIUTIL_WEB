850eaf2f4100772cd4cb2b528da84e8d75a52ac5
Johny-1992 <johnymulenda5@gmail.com>
2026-03-05 09:09:09 +0000

    💎 OMNI_FIX_BUILD: Correction syntaxe JSON-LD pour CMC & Google

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="fr">
-      <head><link rel="icon" href="/logo.png" /><script type="application/ld+json">{"@context":"https://schema.org","@type":"Organization","url":"https://omniutil-web.vercel.app","logo":"https://omniutil-web.vercel.app/logo.png","name":"OMNIUTIL","description":"Infrastructure RWA Souveraine - Ticket CMC #1336140"}</script>
+        <head><link rel="icon" href="/logo.png" /><script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({"@context": "https://schema.org", "@type": "Organization", "url": "https://omniutil-web.vercel.app", "logo": "https://omniutil-web.vercel.app/logo.png", "name": "OMNIUTIL", "description": "Infrastructure RWA Souveraine - Ticket CMC #1336140"}) }} /></head>
         {/* LE LINK ICON EST DÉJÀ DANS METADATA, MAIS ON LE GARDE POUR LA SÉCURITÉ */}
         <link rel="icon" href="/branding/logo.svg" />
         <script
